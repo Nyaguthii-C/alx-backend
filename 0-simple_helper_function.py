4d8556f31e0854bbd61b8b707238805a20e1f7f9
Nyaguthii-C <nyaguthiigc@gmail.com>
2024-01-02 01:33:45 +0300

    simple helper function

---
+++ 0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Helper function"""
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """take in page number  and page sizecand return start and end indices"""
+    return ((page - 1) * page_size, (page * page_size))
