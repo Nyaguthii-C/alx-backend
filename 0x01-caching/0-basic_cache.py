3f6fd965890330ddaff8ef75ba7c7a6e5b5e9874
Nyaguthii-C <nyaguthiigc@gmail.com>
2024-01-04 00:56:27 +0300

    edit script

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -13,6 +13,6 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         """return the value in cache dictionary linked to key"""
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
