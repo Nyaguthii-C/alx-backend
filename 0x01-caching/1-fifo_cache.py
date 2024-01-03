3e70a80aba9d8ae577829b9fe4aa8cb12c834b15
Nyaguthii-C <nyaguthiigc@gmail.com>
2024-01-04 01:38:54 +0300

    implement fifo caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Caching:  FIFO caching"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """inherit from BaseCaching and is a caching system"""
+    def __init__(self):
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """
+        assign item value to key in cache dictionary
+        Update the order of insertion into cache
+        delete first item inserted in cache
+
+        """
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+            self.order.append(key)
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_item = self.order.pop(0)
+            del self.cache_data[first_item]
+
+    def get(self, key):
+        """return the value in cache dictionary linked to key"""
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
