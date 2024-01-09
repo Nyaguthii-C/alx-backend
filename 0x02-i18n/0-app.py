07ed19517953e9e0c9b73006a149a85b4b7ce867
Nyaguthii-C <nyaguthiigc@gmail.com>
2024-01-10 00:57:42 +0300

    render index.html template in the route created

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 """Creating a basic flask app"""
 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 
 @app.route("/")
+def Index():
+    return render_template('index.html')
